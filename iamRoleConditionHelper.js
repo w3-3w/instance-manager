b259d6c61d1e66779fdb8002d287969f60bf769a
w3_3w <w3-3w@live.cn>
2018-05-15 00:47:49 +0900

    minor fix

--- iamRoleConditionHelper.js
+++ iamRoleConditionHelper.js
@@ -2,6 +2,9 @@
 
 const fs = require('fs');
 
+// Serverless does't support variable parsing on keys.
+// This is why this script is needed.
+// https://github.com/serverless/serverless/issues/2892
 module.exports = () => {
   const config = JSON.parse(fs.readFileSync('./config.json'));
   const conditionKey = `ec2:ResourceTag/${config.instance.tag.key}`;
