dfa879749fd8f4003b398e2c88fb61d70d9552de
w3-3w <w3-3w@live.cn>
2018-06-26 17:51:06 +0900

    minor fix

--- helper/iamPolicyConditionHelper.js
+++ helper/iamPolicyConditionHelper.js
@@ -1,16 +1,15 @@
 'use strict';
 
 const fs = require('fs');
+const config = require('../config')();
 
 // This script is for building serverless.yml
 // Serverless doesn't support variable parsing on keys.
 // This script is a workaround.
 // https://github.com/serverless/serverless/issues/2892
 function _(identifier) {
-  return () => {
+  return (sls) => {
     // path is relative to root
-    const configPath = './config.json';
-    const config = JSON.parse(fs.readFileSync(configPath));
     const conditionKey = `${identifier}/${config.tag.key}`;
     const conditionBody = {};
     conditionBody[conditionKey] = false;
